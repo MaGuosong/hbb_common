24c64f259dc97277d45db4c8499a5a696e894b34
MaGuosong <51343128+MaGuosong@users.noreply.github.com>
2026-03-13 11:21:18 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["mgs123.cn"];
+pub const RS_PUB_KEY: &str = "gdS20xoAP+zCJua0rfBB77IthstGIbo4500MYG5BP6s=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
